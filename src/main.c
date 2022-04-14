48948691d2ce6190b651090af169bdc3eda9ed1c
leaf.water <leaf.water@live.com>
2022-04-14 23:32:51 +0300

    демо цикл

--- src/main.c
+++ src/main.c
@@ -1,29 +1,41 @@
 ﻿#include <stdio.h>
 #include"ascii_counter.h"
 
-
+static void _swap_ptr(char** a, char** b) {
+    char *temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 int main(int argc, char* argv[])
 {
     int rc = 0;
     char *data = NULL;
+    char *raw_data = NULL;
+    int it = (argc == 3 && ASCII_IS_NUMERIC(*argv[2]))? atoi(argv[2]): 1;
 
+    if (argc > 1) {
 
-    // есть входной аргумент
-    if (argc > 1) 
-    {
-        data = ascii_parse(argv[1], &rc);
-        if (rc == ASCII_OK) {
-            printf("input:%s output:%s\n", argv[1], data);
-            free(data);
+        printf("%s\n", argv[1]);
+        raw_data = ascii_parse(argv[1], &rc);
+        printf("%s\n", raw_data);
+
+        while (it > 0 && rc == ASCII_OK)
+        {
+            data = ascii_parse(raw_data, &rc);
+            if (rc == ASCII_OK) {
+                printf("%s\n", data);
+                _swap_ptr(&data, &raw_data);
+                free(data);
+            }else
+                printf("error:%d\n", rc);
+            it--;
+
+            usleep(55000);
         }
-        else
-            printf("rc:%d", rc);
+        
     }
-    else 
-    {
-        printf("no input data...\n");
-    }
-
+    else
+       
     return 0;
 }
